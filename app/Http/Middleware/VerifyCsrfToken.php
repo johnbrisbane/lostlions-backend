fb037c9d958d6689f1706f83031511848904af4a
johnbrisbane <52230736+johnbrisbane@users.noreply.github.com>
2022-04-28 19:04:09 -0400

    csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'https://app.lost-lions.com/register/',
+        'https://app.lost-lions.com/api/*',
+        'https://app.lost-lions.com/*'
     ];
 }
